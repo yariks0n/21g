34b4c15ef8afaacbaa27e2cea04aed8b16c49f0e
Yaroslav Kozhumyaka <y.kozhumyaka@gmail.com>
2018-04-17 13:21:35 +0300

    added Animator in MainMenu

--- app/src/main/java/com/droid/ykozh/a21g/Cards.java
+++ app/src/main/java/com/droid/ykozh/a21g/Cards.java
@@ -5,6 +5,7 @@ import android.content.res.AssetManager;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Cards {
 
@@ -31,5 +32,11 @@ public class Cards {
             newCards.add(card);
         }
         return newCards;
+    }
+
+    public static Card getRandomCard() {
+        final Random random = new Random();
+        int pos = random.nextInt(getCards().size()-1);
+        return getCards().get(pos);
     }
 }
--- app/src/main/java/com/droid/ykozh/a21g/MainActivity.java
+++ app/src/main/java/com/droid/ykozh/a21g/MainActivity.java
@@ -1,14 +1,16 @@
 package com.droid.ykozh.a21g;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +18,24 @@ public class MainActivity extends AppCompatActivity {
 
     public MainMenu mMainMenu;
     private Button startButton;
+    private ImageView cardView;
+    private boolean animAcepted;
+
+    private ObjectAnimator scaleXAnimator;
+    private ObjectAnimator scaleYAnimator;
+    private ObjectAnimator scalelXAnimator;
+    private ObjectAnimator scalelYAnimator;
+    private AnimatorSet animatorSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Cards.setCards(this);
+        cardView = (ImageView) findViewById(R.id.card_img);
+        cardView.setImageBitmap(Cards.getRandomCard().getBitmap());
 
-        Display display = getWindowManager().getDefaultDisplay();
+        /*Display display = getWindowManager().getDefaultDisplay();
         // Load the resolution into a Point object
         Point size = new Point();
         display.getSize(size);
@@ -32,10 +43,9 @@ public class MainActivity extends AppCompatActivity {
         mMainMenu = new MainMenu(this,size.x,size.y); // создаём mMainMenu
 
         LinearLayout mainMenuAnimLayout = (LinearLayout) findViewById(R.id.mainMenuAnimLayout); // находим gameLayout
-        mainMenuAnimLayout.addView(mMainMenu);
+        mainMenuAnimLayout.addView(mMainMenu);*/
 
         startButton = (Button)findViewById(R.id.start);
-
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,9 +54,89 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        startAnimation();
+
+    }
+
+    private void startAnimation() {
+        animAcepted = true;
+        scaleXAnimator = ObjectAnimator
+                .ofFloat(cardView, "scaleX", 7)
+                .setDuration(3000);
+
+        scaleYAnimator = ObjectAnimator
+                .ofFloat(cardView, "scaleY", 7)
+                .setDuration(3000);
+
+
+        scalelXAnimator = ObjectAnimator
+                .ofFloat(cardView, "scaleX", 0.1f)
+                .setDuration(3000);
+
+        scalelYAnimator = ObjectAnimator
+                .ofFloat(cardView, "scaleY", 0.1f)
+                .setDuration(3000);
+
+        animatorSet = new AnimatorSet();
+        animatorSet
+                .play(scaleXAnimator)
+                .with(scaleYAnimator)
+                .before(scalelXAnimator)
+                .before(scalelYAnimator);
+        animatorSet.start();
+
+        scalelYAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                if (animAcepted){
+                    cardView.setImageBitmap(Cards.getRandomCard().getBitmap());
+                    animatorSet = new AnimatorSet();
+                    animatorSet
+                            .play(scaleXAnimator)
+                            .with(scaleYAnimator)
+                            .before(scalelXAnimator)
+                            .before(scalelYAnimator);
+                    animatorSet.start();
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        animAcepted = false;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        animAcepted = false;
+    }
+
+    public void onResume() {
+        super.onResume();
+        startAnimation();
+    }
+
+
+    /*@Override
     public void onPause() {
         super.onPause();
         Log.i(TAG, "pause");
@@ -72,6 +162,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Log.i(TAG, "start");
         mMainMenu.resume();
-    }
+    }*/
 
 }
